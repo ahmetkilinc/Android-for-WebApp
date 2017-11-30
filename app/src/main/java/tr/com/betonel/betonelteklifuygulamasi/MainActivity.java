bfd4e1b695c38cdcbc8ffbf7ee6684ff2f8108b4
ahmetkilinc <ahmetkilinc3899@gmail.com>
2017-11-30 17:27:56 +0300

    costum titlebar added.

--- app/src/main/java/tr/com/betonel/betonelteklifuygulamasi/MainActivity.java
+++ app/src/main/java/tr/com/betonel/betonelteklifuygulamasi/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState){
 
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
